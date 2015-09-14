d5325ca2c078b66e7640f3ed335316e95938f9f0
dspecht <specht.dustin@gmail.com>
2015-09-14 01:02:13 -0500

    updated build.bat to fix vim using the hard location that does not exist when I go to look at compile errors

--- build.bat
+++ build.bat
@@ -1,18 +1,17 @@
 @echo off
 cls
 
-set internalProgramFlags=
+set buildDIR=\build\
+set outputDirectories=/Fd.%buildDIR% /Fe.%buildDIR% /Fo.%buildDIR%
 
 set WarningFlags= -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4351
-set Flags= %internalProgramFlags% -Zi -EHsc -nologo -fp:fast -Gm- -GR- -EHa- %WarningFlags%
+set Flags= %internalProgramFlags% -Zi -EHsc -nologo -fp:fast -Gm- -GR- -EHa- %WarningFlags% %outputDirectories%
 
 set LibsLinkedTo= user32.lib Gdi32.lib
 set LinkerFlags= -incremental:no -opt:ref -NODEFAULTLIB:library %LibsLinkedTo%
 
 IF NOT EXIST build mkdir build
-pushd build
 
-del rm *.pdb
+del rm %buildDIR%*.pdb
 
-cl %Flags% ..\win32main.cpp /link %LinkerFlags%
-popd
+cl %Flags% win32main.cpp /link %LinkerFlags%
