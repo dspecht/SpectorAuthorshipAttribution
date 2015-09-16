86900b9e0b4705b25dd82a1640ef154d451f4194
dspecht <specht.dustin@gmail.com>
2015-09-16 02:26:02 -0500

    Build fails for probably a dumb reason but I am tired. putting this up so we can stay similiar on current progress

--- win32main.cpp
+++ win32main.cpp
@@ -23,13 +23,19 @@ WordTypeTag_TDE PopTag(TagStack * Stack)
     int StackIndex = Stack->CurrentTagDepth--;
     return Stack->Tags[StackIndex];
 }
+FILE* OpenFile(char *filePath, char *openFMT="rw")
+{
+    FILE *handle = NULL;
+    fopen_s(&handle, filePath, openFMT);
+    return handle;
+}
 
 void StringReformat(char * Token)
 {
     char CurrentCharacter;
     int Current = 0;
     int LastWrittenTo = 0;
-    
+
     while(Token[Current] != '\0')
     {
         CurrentCharacter = Token[Current++];
@@ -73,7 +79,7 @@ WordTypeTag_TDE FindTag(char * Token, char **tags, TagStack * Stack)
                 i < NUMBER_OF_TAGS;
                 ++i)
             {
-                if(compareString(Token, tags[i]))
+                if(CompareString(Token, tags[i]))
                 {
                     PushTag(WordTypeTag_TDE(i), Stack);
                     return NEW_TAG;
@@ -99,22 +105,22 @@ void ExtractXMLNodeContents(FILE *handle, char **tags)
         {
             StringReformat(Token);
             WordTypeTag_TDE Tag = FindTag(Token, tags, &XMLStack);
-            
+
             if(Tag == NEW_TAG)
             {
-                ActiveTag = XMLStack.Tags[XMLStack.CurrentTagDepth]; 
+                ActiveTag = XMLStack.Tags[XMLStack.CurrentTagDepth];
                 Assert(XMLStack.CurrentTagDepth >= 0);
                 Assert((int)ActiveTag >= (int)FunctionWords_tag && (int)ActiveTag < (int)NUMBER_OF_TAGS);
-                
-                wordsListArray[ActiveTag].count = 0;                
-                continue;   
+
+                wordsListArray[ActiveTag].count = 0;
+                continue;
             }
             else if(Tag == NON_TAG)
             {
                 char *WordStorage = (char *)calloc(1,sizeof(char) * getStringLength(Token));
                 CopyString(Token, WordStorage);
 
-                wordsListArray[ActiveTag].words[wordsListArray[ActiveTag].count++] = WordStorage; 
+                wordsListArray[ActiveTag].words[wordsListArray[ActiveTag].count++] = WordStorage;
             }
             else if(Tag == NULL_TAG)
             {
@@ -131,8 +137,7 @@ bool ReadDictionaryFromXMLConfigFile()
     char *Tags[NUMBER_OF_TAGS] = {"<FunctionWords>", "<Punctuation>", "<Pronoun>",\
         "<Verb>", "<Adverb>", "<Adjective>", "<Preposition>", "<Determiniers>", "<Profanities>"};
 
-    FILE *handle = NULL;
-    fopen_s(&handle, "dictConfig.xml", "r");
+    FILE *handle = OpenFile("dictConfig.xml", "r");
     if(handle == NULL)
     {
         printf("\nFailed to open config file, please ensure the xml file is in \
@@ -148,12 +153,12 @@ bool ReadDictionaryFromXMLConfigFile()
     return true;
 }
 
-bool isPunctuation(char token)
+bool isInATag(char *token, WordTypeTag_TDE tag)
 {
-    // check wordListArray to see if this char in the puncation table
-    for(u32 i = 0; i < wordsListArray[Punctuation_tag].count; i++)
+    u32 i = 0;
+    while(i++ < wordsListArray[tag].count)
     {
-        if((char*)token == wordsListArray[Punctuation_tag].words[i])
+        if(CompareString((char*)token, wordsListArray[tag].words[i]))
         {
             return true;
         }
@@ -169,11 +174,10 @@ inline void AddToDocumentWordCount(DocumentWord *docWordList, u32 docCount, char
     {
         if(docWordList[index].tag == tag)
         {
-            if(docWordList[index].word == word)
+            if(CompareString(docWordList[index].word, word))
             {
                 wordFound = true;
                 docWordList[index].count++;
-                continue;
             }
         }
         index++;
@@ -189,13 +193,7 @@ inline void AddToDocumentWordCount(DocumentWord *docWordList, u32 docCount, char
 
 bool ReadDocument(char *documentFilePath)
 {
-    FILE *handle = NULL;
-    fopen_s(&handle, documentFilePath, "r");
-    if(handle == NULL)
-    {
-        printf("Failed to open text document, check your document path");
-        return false;
-    }
+    FILE *handle = OpenFile(documentFilePath, "r");
     u32 documentWordCount = 0;
     DocumentWord docWordList[MAX_WORDS_IN_A_LIST];
     char *token = (char *)calloc(1, sizeof(char) * LONGEST_WORD_LENGTH);
@@ -204,16 +202,38 @@ bool ReadDocument(char *documentFilePath)
     while(!feof(handle))
     {
         if(fgets(token, LONGEST_WORD_LENGTH, handle))
+        {
             temp = token;
+            bool hasEndingPuncChar = false;
+            u32 length = getStringLength(token);
+            u32 tagProcessingAmount = 0;
             while(*temp++ != '\0') // rethink to actually work
             {
-                if (isPunctuation(temp[0]))
+                if (isInATag((char*)temp[0], Punctuation_tag)
                 {
-                    AddToDocumentWordCount(docWordList, documentWordCount, (char*)temp[0], Punctuation_tag);
+                    AddToDocumentWordCount(docWordList, documentWordCount, temp, Punctuation_tag);
+                    hasEndingPuncChar = true;
+                    continue;
                 }
             }
+            if(hasEndingPuncChar)
+            {
+                SplitString(token, token[length-1], 0, token);
+            }
+            while(tagProcessingAmount++ < NUMBER_OF_TAGS)
+            {
+                if(isInATag(token), tagProcessingAmount)
+                {
+                    AddToDocumentWordCount(docWordList, documentWordCount, token, tagProcessingAmount);
+                }
+             // We need to figure out how we are going to do the other tag with the dynamic list
+             //   else
+             //   {
+             //      AddToDocumentWordCount(docWordList, documentWordCount, token, Other_Tag);
+             //   }
+            }
+        }
     }
-
     return true;
 }
 
